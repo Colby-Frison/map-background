47d45cb1c8f82e816fa8fa7b4947a46b4046b81c
Colby-Frison <colbyfrison100@gmail.com>
2025-01-17 12:32:04 -0600

    Simplified

--- test.py
+++ test.py
@@ -4,49 +4,21 @@ Downloading and Plotting Maps
 
 Plotting maps with Contextily.
 
-Contextily is designed to pull map tile information from the web. In many
-cases we want to go from a location to a map of that location as quickly
-as possible. There are two main ways to do this with Contextily.
-
-Searching for places with text
-==============================
-
-The simplest approach is to search for a location with text. You can do
-this with the ``Place`` class. This will return an object that contains
-metadata about the place, such as its bounding box. It will also contain an
-image of the place.
+This script displays a map of a specified location using Contextily.
 """
-import numpy as np
 import matplotlib.pyplot as plt
 import contextily as cx
 
-loc = cx.Place("boulder", zoom_adjust=0)  # zoom_adjust modifies the auto-zoom
+# Specify the location
+place = "norman"
 
-# Print some metadata
-for attr in ["w", "s", "e", "n", "place", "zoom", "n_tiles"]:
-    print("{}: {}".format(attr, getattr(loc, attr)))
+# Create a Place object for the location
+loc = cx.Place(place, zoom_adjust=1)
 
-# Show the map
-im1 = loc.im
+# Create a single figure
+fig, ax = plt.subplots(figsize=(12, 8))
 
-fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-cx.plot_map(loc, ax=axs[0])
-
-###############################################################################
-# The zoom level will be chosen for you by default, though you can specify
-# this manually as well:
-
-loc2 = cx.Place("boulder", zoom=11)
-cx.plot_map(loc2, ax=axs[1])
-
-###############################################################################
-# Downloading tiles from bounds
-# =============================
-#
-# You can also grab tile information directly from a bounding box + zoom level.
-# This is demoed below:
-
-im2, bbox = cx.bounds2img(loc.w, loc.s, loc.e, loc.n, zoom=loc.zoom, ll=True)
-cx.plot_map(im2, bbox, ax=axs[2], title="Boulder, CO")
+# Plot the map and set the title
+cx.plot_map(loc, ax=ax, title=place.title())
 
 plt.show()
